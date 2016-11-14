e6bd19b2dbc73b6f84433831506d1c1dc4b6775e
Paola Ortiz de Zevallos Villanueva <paolaozv@gmail.com>
2016-11-14 02:07:48 -0500

    cambios proyectos

--- js/app.js
+++ js/app.js
@@ -15,7 +15,43 @@ var cargarPagina = function() {
         	}
     	}, 100);
 	}());
+
+    $("#menu").click(menu);
+    $(".remove").click(menuOcultar);
+    $("#liUno").click(menuOcultar);
+    $("#liDos").click(menuOcultar);
+    $("#liTres").click(menuOcultar);
+    $("#liCuatro").click(menuOcultar);
+    $("#liCinco").click(menuOcultar);
+    $("#js").click(javascript);
+    $("#html").click(html);
+    $("#mobile").click(mobile);
 };
 
-
 $(document).ready(cargarPagina);
+
+var menu = function() {
+    $(".desplegable").show("slow");
+};
+
+var menuOcultar = function() {
+    $(".desplegable").hide("slow");
+};
+
+var javascript = function() {
+    $("#htmlCss").hide("slow");
+    $("#app").hide("slow");
+    $("#javascript").show("slow");
+};
+
+var html = function() {
+    $("#javascript").hide("slow");
+    $("#app").hide("slow");
+    $("#htmlCss").show("slow");
+};
+
+var mobile = function() {
+    $("#javascript").hide("slow");
+    $("#htmlCss").hide("slow");
+    $("#app").show("slow");
+};
